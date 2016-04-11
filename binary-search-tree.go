149df8c147e0c3900709da7358f80d73b046006f
Tom O'Connell <tomoconnell@live.com>
2016-04-10 20:58:39 -0700

    Add breadth first traversal method and tests.

--- binary-search-tree.go
+++ binary-search-tree.go
@@ -1,214 +1,245 @@
 package ds
 
 type node struct {
-  value int
-  left *node
-  right *node
+	value int
+	left  *node
+	right *node
 }
 
 type BST struct {
-  root *node
-  count int
+	root  *node
+	count int
 }
 
 func addNode(n *node, value int) *node {
-  if n == nil {
-    n = new(node)
-    n.value = value
-    return n
-  } else if value < n.value {
-    n.left = addNode(n.left, value)
-  } else {
-    n.right = addNode(n.right, value)
-  }
-  return n
+	if n == nil {
+		n = new(node)
+		n.value = value
+		return n
+	} else if value < n.value {
+		n.left = addNode(n.left, value)
+	} else {
+		n.right = addNode(n.right, value)
+	}
+	return n
 }
 
 func (t *BST) Insert(value int) {
-  t.root = addNode(t.root, value)
-  t.count++
+	t.root = addNode(t.root, value)
+	t.count++
 }
 
 func getNode(n *node, value int) *node {
-  if n == nil {
-    return nil
-  } else if value == n.value {
-    return n
-  } else if value < n.value {
-    return getNode(n.left, value)
-  } else {
-    return getNode(n.right, value)
-  }
+	if n == nil {
+		return nil
+	} else if value == n.value {
+		return n
+	} else if value < n.value {
+		return getNode(n.left, value)
+	} else {
+		return getNode(n.right, value)
+	}
 }
 
 func (t *BST) Contains(value int) bool {
-  if getNode(t.root, value) == nil {
-    return false
-  }
-  return true
+	if getNode(t.root, value) == nil {
+		return false
+	}
+	return true
 }
 
 func getParentNode(n *node, value int) *node {
-  if value < n.value {
-    if value == n.left.value {
-      return n
-    } else {
-      return getParentNode(n.left, value)
-    }
-  } else {
-    if value == n.right.value {
-      return n
-    } else {
-      return getParentNode(n.right, value)
-    }
-  }
+	if value < n.value {
+		if value == n.left.value {
+			return n
+		} else {
+			return getParentNode(n.left, value)
+		}
+	} else {
+		if value == n.right.value {
+			return n
+		} else {
+			return getParentNode(n.right, value)
+		}
+	}
 }
 
 func getParent(t *BST, value int) *node {
-  if value == t.root.value || t.count < 2 {
-    return nil
-  }
-  return getParentNode(t.root, value)
+	if value == t.root.value || t.count < 2 {
+		return nil
+	}
+	return getParentNode(t.root, value)
 }
 
 func (t *BST) Remove(value int) bool {
-  nodeToRemove := getNode(t.root, value)
+	nodeToRemove := getNode(t.root, value)
 
-  if nodeToRemove == nil {
-    return false
-  }
+	if nodeToRemove == nil {
+		return false
+	}
 
-  if t.count == 1 {
-    t.root = nil
-    t.count = 0
-    return true
-  }
+	if t.count == 1 {
+		t.root = nil
+		t.count = 0
+		return true
+	}
 
-  parentNode := getParent(t, value)
+	parentNode := getParent(t, value)
 
-  // Covers edge case; removing the root and there is more than 1 node.
-  if parentNode == nil {
-    if t.root.left == nil && t.root.right != nil {
-      t.root = t.root.right
-    } else if t.root.left != nil && t.root.right == nil {
-      t.root = t.root.left
-    } else {
-      largestValueOnLeft := t.root.left
-      for largestValueOnLeft.right != nil {
-        largestValueOnLeft = largestValueOnLeft.right
-      }
-      largetValueOnLeftParent := getParent(t, largestValueOnLeft.value)
-      t.root.value = largestValueOnLeft.value
-      largetValueOnLeftParent.right = nil
-    }
-    t.count--
-    return true
-  }
+	// Covers edge case; removing the root and there is more than 1 node.
+	if parentNode == nil {
+		if t.root.left == nil && t.root.right != nil {
+			t.root = t.root.right
+		} else if t.root.left != nil && t.root.right == nil {
+			t.root = t.root.left
+		} else {
+			largestValueOnLeft := t.root.left
+			for largestValueOnLeft.right != nil {
+				largestValueOnLeft = largestValueOnLeft.right
+			}
+			largetValueOnLeftParent := getParent(t, largestValueOnLeft.value)
+			t.root.value = largestValueOnLeft.value
+			largetValueOnLeftParent.right = nil
+		}
+		t.count--
+		return true
+	}
 
-  if nodeToRemove.left == nil && nodeToRemove.right == nil {
-    if nodeToRemove.value < parentNode.value {
-      parentNode.left = nil
-    } else {
-      parentNode.right = nil
-    }
-  } else if nodeToRemove.left == nil && nodeToRemove.right != nil {
-    if nodeToRemove.value < parentNode.value {
-      parentNode.left = nodeToRemove.right
-    } else {
-      parentNode.right = nodeToRemove.right
-    }
-  } else if nodeToRemove.left != nil && nodeToRemove.right == nil {
-    if nodeToRemove.value < parentNode.value {
-      parentNode.left = nodeToRemove.left
-    } else {
-      parentNode.right = nodeToRemove.left
-    }
-  } else {
-    largestValueNode := nodeToRemove.left
+	if nodeToRemove.left == nil && nodeToRemove.right == nil {
+		if nodeToRemove.value < parentNode.value {
+			parentNode.left = nil
+		} else {
+			parentNode.right = nil
+		}
+	} else if nodeToRemove.left == nil && nodeToRemove.right != nil {
+		if nodeToRemove.value < parentNode.value {
+			parentNode.left = nodeToRemove.right
+		} else {
+			parentNode.right = nodeToRemove.right
+		}
+	} else if nodeToRemove.left != nil && nodeToRemove.right == nil {
+		if nodeToRemove.value < parentNode.value {
+			parentNode.left = nodeToRemove.left
+		} else {
+			parentNode.right = nodeToRemove.left
+		}
+	} else {
+		largestValueNode := nodeToRemove.left
 
-    for largestValueNode.right != nil {
-      largestValueNode = largestValueNode.right
-    }
-    largestValueParent := getParentNode(nodeToRemove, largestValueNode.value)
-    nodeToRemove.value = largestValueNode.value
-    largestValueParent.right = nil
-  }
-  t.count--
-  return true
+		for largestValueNode.right != nil {
+			largestValueNode = largestValueNode.right
+		}
+		largestValueParent := getParentNode(nodeToRemove, largestValueNode.value)
+		nodeToRemove.value = largestValueNode.value
+		largestValueParent.right = nil
+	}
+	t.count--
+	return true
 }
 
 func findMaxNode(n *node) int {
-  if n.right == nil {
-    return n.value
-  } else {
-    return findMaxNode(n.right)
-  }
+	if n.right == nil {
+		return n.value
+	} else {
+		return findMaxNode(n.right)
+	}
 }
 
 func (t *BST) Max() int {
-  return findMaxNode(t.root)
+	return findMaxNode(t.root)
 }
 
 func findMinNode(n *node) int {
-  if n.left == nil {
-    return n.value
-  } else {
-    return findMinNode(n.left)
-  }
+	if n.left == nil {
+		return n.value
+	} else {
+		return findMinNode(n.left)
+	}
 }
 
 func (t *BST) Min() int {
-  return findMinNode(t.root)
+	return findMinNode(t.root)
 }
 
 // Use to process data.
 type process func(int)
 
 func preOrderNodeTraversal(n *node, fn process) {
-  fn(n.value)
+	fn(n.value)
 
-  if n.left != nil {
-    preOrderNodeTraversal(n.left, fn)
-  }
-  if n.right != nil {
-    preOrderNodeTraversal(n.right, fn)
-  }
+	if n.left != nil {
+		preOrderNodeTraversal(n.left, fn)
+	}
+	if n.right != nil {
+		preOrderNodeTraversal(n.right, fn)
+	}
 }
 
 func (t *BST) PreOrder(fn process) {
-  if t.root != nil {
-    preOrderNodeTraversal(t.root, fn)
-  }
+	if t.root != nil {
+		preOrderNodeTraversal(t.root, fn)
+	}
 }
 
 func postOrderTraversal(n *node, fn process) {
-  if n.left != nil {
-    postOrderTraversal(n.left, fn)
-  }
-  if n.right != nil {
-    postOrderTraversal(n.right, fn)
-  }
-  fn(n.value)
+	if n.left != nil {
+		postOrderTraversal(n.left, fn)
+	}
+	if n.right != nil {
+		postOrderTraversal(n.right, fn)
+	}
+	fn(n.value)
 }
 
 func (t *BST) PostOrder(fn process) {
-  if t.root != nil {
-    postOrderTraversal(t.root, fn)
-  }
+	if t.root != nil {
+		postOrderTraversal(t.root, fn)
+	}
 }
 
 func inOrderTraversal(n *node, fn process) {
-  if n.left != nil {
-    inOrderTraversal(n.left, fn)
-  }
-  fn(n.value)
-  if n.right != nil {
-    inOrderTraversal(n.right, fn)
-  }
+	if n.left != nil {
+		inOrderTraversal(n.left, fn)
+	}
+	fn(n.value)
+	if n.right != nil {
+		inOrderTraversal(n.right, fn)
+	}
 }
 
 func (t *BST) InOrder(fn process) {
-  if t.root != nil {
-    inOrderTraversal(t.root, fn)
-  }
+	if t.root != nil {
+		inOrderTraversal(t.root, fn)
+	}
+}
+
+func breadthFirstTraversal(n *node, fn process) {
+	queue := []*node{}
+	current := n
+
+	for current != nil {
+		fn(current.value)
+
+		if current.left != nil {
+			queue = append(queue, current.left)
+		}
+
+		if current.right != nil {
+			queue = append(queue, current.right)
+		}
+
+		length := len(queue)
+		if length != 0 {
+			current = queue[0]
+			queue = queue[1:]
+		} else {
+			current = nil
+		}
+	}
+}
+
+func (t *BST) BreadthFirst(fn process) {
+	if t.root != nil {
+		breadthFirstTraversal(t.root, fn)
+	}
 }
--- binary-search-tree_test.go
+++ binary-search-tree_test.go
@@ -3,135 +3,155 @@ package ds
 import "testing"
 
 func TestInsert(t *testing.T) {
-  bst := new(BST)
+	bst := new(BST)
 
-  bst.Insert(1)
-  bst.Insert(300)
-  bst.Insert(18)
-  bst.Insert(45)
-  bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(300)
+	bst.Insert(18)
+	bst.Insert(45)
+	bst.Insert(2)
 
-  if bst.Contains(18) != true {
-    t.Errorf("bst.Contains(18) should be true")
-  }
+	if bst.Contains(18) != true {
+		t.Errorf("bst.Contains(18) should be true")
+	}
 
-  bst.Insert(2)
+	bst.Insert(2)
 
-  if bst.Contains(2) != true {
-    t.Errorf("bst.Contains(2) should be true")
-  }
+	if bst.Contains(2) != true {
+		t.Errorf("bst.Contains(2) should be true")
+	}
 }
 
 func TestRemove(t *testing.T) {
-  bst := new(BST)
+	bst := new(BST)
 
-  bst.Insert(1)
-  bst.Insert(300)
-  bst.Insert(18)
-  bst.Insert(45)
-  bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(300)
+	bst.Insert(18)
+	bst.Insert(45)
+	bst.Insert(2)
 
-  if bst.Contains(300) != true {
-    t.Errorf("bst.Contains(300) should be true")
-  }
-  if bst.Contains(2) != true {
-    t.Errorf("bst.Contains(2) should be true")
-  }
+	if bst.Contains(300) != true {
+		t.Errorf("bst.Contains(300) should be true")
+	}
+	if bst.Contains(2) != true {
+		t.Errorf("bst.Contains(2) should be true")
+	}
 
-  bst.Remove(300)
-  if bst.Contains(300) == true {
-    t.Errorf("bst.Contains(300) should be false")
-  }
-  bst.Remove(2)
-  if bst.Contains(2) == true {
-    t.Errorf("bst.Contains(2) should be false")
-  }
+	bst.Remove(300)
+	if bst.Contains(300) == true {
+		t.Errorf("bst.Contains(300) should be false")
+	}
+	bst.Remove(2)
+	if bst.Contains(2) == true {
+		t.Errorf("bst.Contains(2) should be false")
+	}
 }
 
 func TestMax(t *testing.T) {
-  bst := new(BST)
+	bst := new(BST)
 
-  bst.Insert(1)
-  bst.Insert(300)
-  bst.Insert(18)
-  bst.Insert(45)
-  bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(300)
+	bst.Insert(18)
+	bst.Insert(45)
+	bst.Insert(2)
 
-  if bst.Max() != 300 {
-    t.Errorf("bst.Max() should be 300")
-  }
+	if bst.Max() != 300 {
+		t.Errorf("bst.Max() should be 300")
+	}
 }
 
 func TestMin(t *testing.T) {
-  bst := new(BST)
+	bst := new(BST)
 
-  bst.Insert(1)
-  bst.Insert(300)
-  bst.Insert(18)
-  bst.Insert(45)
-  bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(300)
+	bst.Insert(18)
+	bst.Insert(45)
+	bst.Insert(2)
 
-  if bst.Min() != 1 {
-    t.Errorf("bst.Min() should be 1")
-  }
+	if bst.Min() != 1 {
+		t.Errorf("bst.Min() should be 1")
+	}
 }
 
 func TestPreOrder(t *testing.T) {
-  bst := new(BST)
-  items := []int{}
-  bst.Insert(2)
-  bst.Insert(1)
-  bst.Insert(3)
+	bst := new(BST)
+	items := []int{}
+	bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(3)
 
-  bst.PreOrder(func (x int) { items = append(items, x) })
+	bst.PreOrder(func(x int) { items = append(items, x) })
 
-  if items[0] != 2 {
-    t.Errorf("items[0] should be 2")
-  }
-  if items[1] != 1 {
-    t.Errorf("items[1] should be 1")
-  }
-  if items[2] != 3 {
-    t.Errorf("items[2] should be 3")
-  }
+	if items[0] != 2 {
+		t.Errorf("items[0] should be 2")
+	}
+	if items[1] != 1 {
+		t.Errorf("items[1] should be 1")
+	}
+	if items[2] != 3 {
+		t.Errorf("items[2] should be 3")
+	}
 }
 
 func TestPostOrder(t *testing.T) {
-  bst := new(BST)
-  items := []int{}
-  bst.Insert(2)
-  bst.Insert(1)
-  bst.Insert(3)
+	bst := new(BST)
+	items := []int{}
+	bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(3)
 
-  bst.PostOrder(func (x int) { items = append(items, x) })
+	bst.PostOrder(func(x int) { items = append(items, x) })
 
-  if items[0] != 1 {
-    t.Errorf("items[0] should be 1")
-  }
-  if items[1] != 3 {
-    t.Errorf("items[1] should be 3")
-  }
-  if items[2] != 2 {
-    t.Errorf("items[2] should be 2")
-  }
+	if items[0] != 1 {
+		t.Errorf("items[0] should be 1")
+	}
+	if items[1] != 3 {
+		t.Errorf("items[1] should be 3")
+	}
+	if items[2] != 2 {
+		t.Errorf("items[2] should be 2")
+	}
 }
 
 func TestInOrder(t *testing.T) {
-  bst := new(BST)
-  items := []int{}
-  bst.Insert(2)
-  bst.Insert(1)
-  bst.Insert(3)
+	bst := new(BST)
+	items := []int{}
+	bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(3)
 
-  bst.InOrder(func (x int) { items = append(items, x) })
+	bst.InOrder(func(x int) { items = append(items, x) })
 
-  if items[0] != 1 {
-    t.Errorf("items[0] should be 1")
-  }
-  if items[1] != 2 {
-    t.Errorf("items[1] should be 2")
-  }
-  if items[2] != 3 {
-    t.Errorf("items[2] should be 3")
-  }
+	if items[0] != 1 {
+		t.Errorf("items[0] should be 1")
+	}
+	if items[1] != 2 {
+		t.Errorf("items[1] should be 2")
+	}
+	if items[2] != 3 {
+		t.Errorf("items[2] should be 3")
+	}
+}
+
+func TestBreadthFirst(t *testing.T) {
+	bst := new(BST)
+	items := []int{}
+	bst.Insert(2)
+	bst.Insert(1)
+	bst.Insert(3)
+
+	bst.BreadthFirst(func(x int) { items = append(items, x) })
+
+	if items[0] != 2 {
+		t.Errorf("items[0] should be 2")
+	}
+	if items[1] != 1 {
+		t.Errorf("items[1] should be 1, got %d", items[1])
+	}
+	if items[2] != 3 {
+		t.Errorf("items[2] should be 3 got %d", items[2])
+	}
 }
